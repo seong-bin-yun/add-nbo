a09a39d2656eb0801b0208806844a2f6b34e0b17
seongbin <tbvjaos3634@gmail.com>
2023-07-18 10:48:03 -0400

    Add add modules

---
+++ add.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <stdint.h>
+
+uint32_t add(uint32_t a,uint32_t b){
+        uint32_t res = a + b;
+	return res;
+}
+
---
+++ add.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdint.h>
+
+uint32_t add(uint32_t, uint32_t);
